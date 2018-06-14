1563eeae6cafd3b65ed612debec3fd7ac69d5490
LeeXujie <1808511884@qq.com>
2018-06-14 10:13:15 +0800

    first commit

---
+++ img2video.cpp
@@ -0,0 +1,85 @@
+/**
+  @file videowriter_basic.cpp
+  @brief A very basic sample for using VideoWriter and VideoCapture
+  @author PkLab.net
+  @date Aug 24, 2016
+*/
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+#include <stdio.h>
+
+using namespace cv;
+using namespace std;
+
+static void help(char** argv)
+{
+    cout << "\nThis sample shows you how to read a sequence of images using the VideoCapture interface.\n"
+         << "Usage: " << argv[0] << " <image_mask> (example mask: example_%02d.jpg) <video_name> (example: video.avi) <video_fps> (example: 25.0)\n"
+         << "Image mask defines the name variation for the input images that have to be read as a sequence. \n"
+         << "Using the mask example_%02d.jpg will read in images labeled as 'example_00.jpg', 'example_01.jpg', etc."
+         << endl;
+}
+
+int main(int argc, char** argv)
+{
+    if(argc<4){ help(argv); return false; }
+
+    cv::CommandLineParser parser(argc, argv, "{@image| ../data/left%02d.jpg |}");
+    string first_file = parser.get<string>("@image");
+
+    if(first_file.empty())
+    {
+        return 1;
+    }
+
+    VideoCapture sequence(first_file);
+
+    if (!sequence.isOpened())
+    {
+        cerr << "Failed to open the image sequence!\n" << endl;
+        return 1;
+    }
+
+    Mat src;
+
+    // get one frame from camera to know frame size and type
+    sequence >> src;
+    // check if we succeeded
+    if (src.empty()) {
+        cerr << "ERROR! blank frame grabbed\n";
+        return -1;
+    }
+    bool isColor = (src.type() == CV_8UC3);
+
+    //--- INITIALIZE VIDEOWRITER
+    VideoWriter writer;
+    int codec = CV_FOURCC('M', 'J', 'P', 'G');  // select desired codec (must be available at runtime)
+    double fps = atof(argv[3]);                 // framerate of the created video stream
+    string filename = "./";
+    filename += argv[2];                        // name of the output video file
+    writer.open(filename, codec, fps, src.size(), isColor);
+    // check if we succeeded
+    if (!writer.isOpened()) {
+        cerr << "Could not open the output video file for write\n";
+        return -1;
+    }
+
+    //--- GRAB AND WRITE LOOP
+    cout << "Writing videofile: " << filename << endl;
+    for (;;)
+    {
+        // check if we succeeded
+        if (!sequence.read(src)) {
+            cerr << "End of Sequence\n";
+            break;
+        }
+        // encode the frame into the videofile stream
+        writer.write(src);
+        // show live and wait for a key with timeout long enough to show images
+        imshow("Image sequence | press ESC to close", src);
+        if (waitKey(10) >= 0)break;
+    }
+    // the videofile will be closed and released automatically in VideoWriter destructor
+    return 0;
+}
